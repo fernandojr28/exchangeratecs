62940cdd37aedce6080bb650f5e849db6921823c
fernandojr28 <fernandojr280897@gmail.com>
2021-12-10 15:19:39 -0500

    make it better

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const rates = require("./rates")
 
+app.use(cors())
 
-app.listen(process.env.PORT || 8080, 
-	() => console.log("Server is running..."));
-
+app.listen(process.env.PORT || 8080, () => console.log("Server is running..."));
 
 
 app.get('/api/rates', (req, res) => {
